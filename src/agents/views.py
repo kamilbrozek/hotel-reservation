9af80a40c449fae72a4ebf1254f45f4a66ef724e
Kamil Brożek <kamilbrozek@localhost.localdomain>
2020-01-23 22:19:15 +0100

    Add agent model

---
+++ src/agents/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from .models import Agent
+
+# Create your views here.
+
+def agents_list(request):
+    agents_list = Agent.objects.all()
+    template = "agents/agents.html" 
+    context = {
+        'agents_list' : agents_list
+    }
+
+    return render(request,template,context)
--- src/project/urls.py
+++ src/project/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('property/', include('property.urls',namespace='property')),
+    path('agents/', include('agents.urls',namespace='agents')),
     
 ]
 
