ca51cb85df1bed3c228222de206b679e3172df51
kamilbrozek <kamil.brozek@hotmail.com>
2020-03-24 22:28:49 +0100

    property list fix

--- src/contact/forms.py
+++ src/contact/forms.py
@@ -2,6 +2,16 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    subject = forms.CharField()
-    email_from = forms.EmailField(required=True)
-    message = forms.CharField(widget=forms.Textarea(),required=True)
+    subject = forms.CharField(
+        label= "Subject",
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    email_from = forms.EmailField(
+        required=True,
+        label="Email",
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+        )
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={'class': 'form-control'}),
+        required=True
+        )
--- src/property/models.py
+++ src/property/models.py
@@ -2,21 +2,22 @@ from django.db import models
 
 # Create your models here.
 
-property_type =(
-    ('S', "sale"),
-    ('R', "rent")
+property_types =(
+    ('Sale', "sale"),
+    ('Rent', "rent")
 )
 
 class Property(models.Model):
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=50, null=True)
-    property_type = models.CharField(choices=property_type,max_length=10)
+    property_type = models.CharField(choices=property_types,max_length=10)
     price = models.PositiveIntegerField()
     area = models.DecimalField(decimal_places=2, max_digits=5)
     beds_number = models.PositiveIntegerField()
     bath_number = models.PositiveIntegerField()
     garages_number = models.PositiveIntegerField()
     image = models.ImageField(upload_to='property/',null=True)
+    details = models.TextField(max_length= 300)
     category = models.ForeignKey('Category', null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
